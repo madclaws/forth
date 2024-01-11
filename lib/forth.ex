43cf6f4e7e6fa0b312cf2867b3c1a079f14eb1c8
madclaws <pavanan.anandu63@gmail.com>
2024-01-11 14:30:14 +0530

    fix: passing forth struct instead of stack only to downstream functions

--- lib/forth.ex
+++ lib/forth.ex
@@ -5,10 +5,11 @@ defmodule Forth do
 
   # TODO: typedoc
   @type t :: %__MODULE__{
-          stack: list(integer())
+          stack: list(integer()),
+          custom_words: map()
         }
 
-  defstruct stack: []
+  defstruct stack: [], custom_words: %{}
 
   @spec new :: __MODULE__.t()
   def new do
@@ -18,32 +19,37 @@ defmodule Forth do
   @spec stack(__MODULE__.t()) :: list(integer())
   def stack(%__MODULE__{stack: stack}), do: stack
 
+  @doc """
+
+  NOTE: Stack pushes are prepended than appended for faster and easier list manipulations.
+  After the evaluation, the stack list is reversed.
+  """
   @spec eval(__MODULE__.t(), String.t()) :: {:ok, __MODULE__.t()} | {:error, String.t()}
-  def eval(%__MODULE__{stack: stack}, input) do
+  def eval(%__MODULE__{} = forth, input) do
     String.split(input)
-    |> Enum.reduce_while(stack, fn data, stack ->
-      case eval_token(stack, data) do
-        res when is_binary(res) -> {:halt, {:error, res}}
-        res -> {:cont, res}
+    |> Enum.reduce_while(forth, fn data, forth ->
+      case eval_token(forth, data) do
+        %__MODULE__{} = res -> {:cont, res}
+        res -> {:halt, {:error, res}}
       end
     end)
     |> case do
-      res when is_list(res) ->
-        {:ok, %__MODULE__{stack: Enum.reverse(res)}}
+      %__MODULE__{} = res ->
+        {:ok, %{res | stack: Enum.reverse(res.stack)}}
 
       err ->
         err
     end
   end
 
-  @spec eval_token(stack :: list(), token :: String.t()) :: list(integer()) | String.t()
-  defp eval_token(stack, token) do
+  @spec eval_token(__MODULE__.t(), token :: String.t()) :: list(integer()) | String.t()
+  defp eval_token(%__MODULE__{stack: stack} = forth, token) do
     cond do
       number?(token) ->
-        [String.to_integer(token) | stack]
+        %{forth | stack: [String.to_integer(token) | stack]}
 
       true ->
-        eval_word(stack, String.downcase(token))
+        eval_word(forth, String.downcase(token))
     end
   end
 
@@ -55,36 +61,46 @@ defmodule Forth do
   end
 
   # TODO: Add the required stack size check
-  @spec eval_word(list(integer()), String.t()) :: stack :: list(integer()) | String.t()
-  defp eval_word(stack, token) when token in ["+", "-", "/", "*"] do
-    eval_arithmetic(token, stack)
+  @spec eval_word(__MODULE__.t(), String.t()) :: stack :: __MODULE__.t() | String.t()
+  defp eval_word(%__MODULE__{stack: stack} = forth, token) when token in ["+", "-", "/", "*"] do
+    with [_ | _] = stack <- eval_arithmetic(token, stack) do
+      %{forth | stack: stack}
+    end
   end
 
-  defp eval_word([], "dup"), do: "Not enough elements in stack for operaiton: DUP"
+  defp eval_word(%__MODULE__{stack: []}, "dup"),
+    do: "Not enough elements in stack for operaiton: DUP"
 
-  defp eval_word([a | _] = stack, "dup") do
-    [a | stack]
+  defp eval_word(%__MODULE__{stack: [a | _]} = forth, "dup") do
+    %{forth | stack: [a | forth.stack]}
   end
 
-  defp eval_word([], "drop"), do: "Not enough elements in stack for operaiton: DROP"
+  defp eval_word(%__MODULE__{stack: []}, "drop"),
+    do: "Not enough elements in stack for operaiton: DROP"
 
-  defp eval_word([_ | t] = _stack, "drop") do
-    t
+  defp eval_word(%__MODULE__{stack: [_ | t]} = forth, "drop") do
+    %{forth | stack: t}
   end
 
-  defp eval_word([b, a | _] = stack, "swap") do
-    List.replace_at(stack, 0, a)
-    |> List.replace_at(1, b)
+  defp eval_word(%__MODULE__{stack: [b, a | _]} = forth, "swap") do
+    %{
+      forth
+      | stack:
+          List.replace_at(forth.stack, 0, a)
+          |> List.replace_at(1, b)
+    }
   end
 
-  defp eval_word(_stack, "swap"), do: "Not enough elements in stack for operaiton: SWAP"
+  defp eval_word(_forth, "swap"), do: "Not enough elements in stack for operaiton: SWAP"
 
-  defp eval_word([_b, a | _] = stack, "over") do
-    [a | stack]
+  defp eval_word(%__MODULE__{stack: [_b, a | _]} = forth, "over") do
+    %{forth | stack: [a | forth.stack]}
   end
 
-  defp eval_word(_stack, "over"), do: "Not enough elements in stack for operaiton: OVER"
+  defp eval_word(_forth, "over"), do: "Not enough elements in stack for operaiton: OVER"
 
+  defp eval_word(%__MODULE__{} = forth, ":") do
+  end
 
   defp eval_word(_, _), do: "No OP defined"
 
@@ -97,12 +113,15 @@ defmodule Forth do
       "+" ->
         List.delete_at(stack, 0)
         |> List.replace_at(0, a + b)
+
       "-" ->
         List.delete_at(stack, 0)
         |> List.replace_at(0, a - b)
+
       "*" ->
         List.delete_at(stack, 0)
         |> List.replace_at(0, a * b)
+
       "/" ->
         if b == 0 do
           "Division by zero is invalid"
--- test/forth_test.exs
+++ test/forth_test.exs
@@ -9,110 +9,136 @@ defmodule ForthTest do
   end
 
   test "addition, success" do
-    assert {:ok, %{stack: [3]}} = Forth.new()
-    |> Forth.eval("1 2 +")
+    assert {:ok, %{stack: [3]}} =
+             Forth.new()
+             |> Forth.eval("1 2 +")
   end
 
   test "addition, failed" do
-    assert {:error, _} = Forth.new()
-    |> Forth.eval("1 +")
+    assert {:error, _} =
+             Forth.new()
+             |> Forth.eval("1 +")
   end
 
   test "subtraction, success" do
-    assert {:ok, %{stack: [-8]}} = Forth.new()
-    |> Forth.eval("2 10 -")
+    assert {:ok, %{stack: [-8]}} =
+             Forth.new()
+             |> Forth.eval("2 10 -")
   end
 
   test "subtraction, failed" do
-    assert {:error, _} = Forth.new()
-    |> Forth.eval("10 -")
+    assert {:error, _} =
+             Forth.new()
+             |> Forth.eval("10 -")
   end
 
   test "multiplication, success" do
-    assert {:ok, %{stack: [40]}} = Forth.new()
-    |> Forth.eval("20 2 *")
+    assert {:ok, %{stack: [40]}} =
+             Forth.new()
+             |> Forth.eval("20 2 *")
   end
 
   test "multiplication, failed" do
-    assert {:error, _} = Forth.new()
-    |> Forth.eval("20 *")
+    assert {:error, _} =
+             Forth.new()
+             |> Forth.eval("20 *")
   end
 
   test "division, success" do
-    assert {:ok, %{stack: [10]}} = Forth.new()
-    |> Forth.eval("20 2 /")
+    assert {:ok, %{stack: [10]}} =
+             Forth.new()
+             |> Forth.eval("20 2 /")
   end
 
   test "division, failed" do
-    assert {:error, "Division by zero is invalid"} = Forth.new()
-    |> Forth.eval("20 0 /")
+    assert {:error, "Division by zero is invalid"} =
+             Forth.new()
+             |> Forth.eval("20 0 /")
   end
 
   test "multiple arithmetic operations" do
-    assert {:ok, %{stack: [-10]}} = Forth.new()
-    |> Forth.eval("10 20 + 40 -")
+    assert {:ok, %{stack: [-10]}} =
+             Forth.new()
+             |> Forth.eval("10 20 + 40 -")
 
-    assert {:ok, %{stack: [2]}} = Forth.new()
-    |> Forth.eval("2 4 + 3 /")
+    assert {:ok, %{stack: [2]}} =
+             Forth.new()
+             |> Forth.eval("2 4 + 3 /")
 
-    assert {:ok, %{stack: [5]}} = Forth.new()
-    |> Forth.eval("10 20 * 40 /")
+    assert {:ok, %{stack: [5]}} =
+             Forth.new()
+             |> Forth.eval("10 20 * 40 /")
 
-    assert {:error, _} = Forth.new()
-    |> Forth.eval("10 20 * 0 /")
+    assert {:error, _} =
+             Forth.new()
+             |> Forth.eval("10 20 * 0 /")
   end
 
   test "DUP operations" do
-    assert {:ok, %{stack: [1, 1]}} = Forth.new()
-    |> Forth.eval("1 DUP")
+    assert {:ok, %{stack: [1, 1]}} =
+             Forth.new()
+             |> Forth.eval("1 DUP")
 
-    assert {:ok, %{stack: [10, 30, 30]}} = Forth.new()
-    |> Forth.eval("10 30 dup")
+    assert {:ok, %{stack: [10, 30, 30]}} =
+             Forth.new()
+             |> Forth.eval("10 30 dup")
 
-    assert {:ok, %{stack: [10, 10, 10, 10]}} = Forth.new()
-    |> Forth.eval("10 DUP dup DUp")
+    assert {:ok, %{stack: [10, 10, 10, 10]}} =
+             Forth.new()
+             |> Forth.eval("10 DUP dup DUp")
 
     # dup failed
-    assert {:error, "Not enough elements in stack for operaiton: DUP"} = Forth.new()
-    |> Forth.eval("DUP")
+    assert {:error, "Not enough elements in stack for operaiton: DUP"} =
+             Forth.new()
+             |> Forth.eval("DUP")
   end
 
   test "DROP operations" do
-    assert {:ok, %{stack: []}} = Forth.new()
-    |> Forth.eval("1 drop")
+    assert {:ok, %{stack: []}} =
+             Forth.new()
+             |> Forth.eval("1 drop")
 
-    assert {:ok, %{stack: [10]}} = Forth.new()
-    |> Forth.eval("10 20 30 40 DROP DROP drop")
+    assert {:ok, %{stack: [10]}} =
+             Forth.new()
+             |> Forth.eval("10 20 30 40 DROP DROP drop")
 
     # drop failed
-    assert {:error, "Not enough elements in stack for operaiton: DROP"} = Forth.new()
-    |> Forth.eval("10 20 DROP DROP drop")
+    assert {:error, "Not enough elements in stack for operaiton: DROP"} =
+             Forth.new()
+             |> Forth.eval("10 20 DROP DROP drop")
   end
 
   test "SWAP operations" do
-    assert {:ok, %{stack: [2, 1]}} = Forth.new()
-    |> Forth.eval("1 2 swap")
+    assert {:ok, %{stack: [2, 1]}} =
+             Forth.new()
+             |> Forth.eval("1 2 swap")
 
-    assert {:ok, %{stack: [10, 20, 40, 30]}} = Forth.new()
-    |> Forth.eval("10 20 30 40 SWAP")
+    assert {:ok, %{stack: [10, 20, 40, 30]}} =
+             Forth.new()
+             |> Forth.eval("10 20 30 40 SWAP")
 
-    assert {:ok, %{stack: [20, 30, 40, 10]}} = Forth.new()
-    |> Forth.eval("10 20 swap 30 swap 40 swap")
+    assert {:ok, %{stack: [20, 30, 40, 10]}} =
+             Forth.new()
+             |> Forth.eval("10 20 swap 30 swap 40 swap")
 
     # swap failed
-    assert {:error, "Not enough elements in stack for operaiton: SWAP"} = Forth.new()
-    |> Forth.eval("10 swap swap SWAP")
+    assert {:error, "Not enough elements in stack for operaiton: SWAP"} =
+             Forth.new()
+             |> Forth.eval("10 swap swap SWAP")
   end
 
   test "OVER operations" do
-    assert {:ok, %{stack: [20, 30, 20]}} = Forth.new()
-    |> Forth.eval("20 30 over")
+    assert {:ok, %{stack: [20, 30, 20]}} =
+             Forth.new()
+             |> Forth.eval("20 30 over")
 
-    assert {:ok, %{stack: [10, 20, 10, 20, 10]}} = Forth.new()
-    |> Forth.eval("10 20 over Over OVEr")
+    assert {:ok, %{stack: [10, 20, 10, 20, 10]}} =
+             Forth.new()
+             |> Forth.eval("10 20 over Over OVEr")
 
     # over failed
-    assert {:error, "Not enough elements in stack for operaiton: OVER"} = Forth.new()
-    |> Forth.eval("10 over")
+    assert {:error, "Not enough elements in stack for operaiton: OVER"} =
+             Forth.new()
+             |> Forth.eval("10 over")
   end
 end
