5edce5890184e4c1bae0da757466ae078ed00396
madclaws <pavanan.anandu63@gmail.com>
2024-01-11 16:51:03 +0530

    feat: added ability to add user defined words

--- lib/forth.ex
+++ lib/forth.ex
@@ -43,11 +43,11 @@ defmodule Forth do
 
   defp eval_token(%__MODULE__{stack: _stack, tokens: []} = forth), do: forth
 
-  defp eval_token(%__MODULE__{stack: stack, tokens: [token | rest]} = forth)
+  defp eval_token(%__MODULE__{stack: stack, tokens: [token | next]} = forth)
        when token in ["+", "-", "/", "*"] do
     case eval_arithmetic(token, stack) do
       [_ | _] = stack ->
-        eval_token(%{forth | stack: stack, tokens: rest})
+        eval_token(%{forth | stack: stack, tokens: next})
 
       err ->
         eval_token(err)
@@ -57,41 +57,80 @@ defmodule Forth do
   defp eval_token(%__MODULE__{stack: [], tokens: ["dup" | _]}),
     do: eval_token("Not enough elements in stack for operaiton: DUP")
 
-  defp eval_token(%__MODULE__{stack: [a | _], tokens: ["dup" | rest]} = forth) do
-    eval_token(%{forth | stack: [a | forth.stack], tokens: rest})
+  defp eval_token(%__MODULE__{stack: [a | _], tokens: ["dup" | next]} = forth) do
+    eval_token(%{forth | stack: [a | forth.stack], tokens: next})
   end
 
   defp eval_token(%__MODULE__{stack: [], tokens: ["drop" | _]}),
     do: eval_token("Not enough elements in stack for operaiton: DROP")
 
-  defp eval_token(%__MODULE__{stack: [_ | t], tokens: ["drop" | rest]} = forth) do
-    eval_token(%{forth | stack: t, tokens: rest})
+  defp eval_token(%__MODULE__{stack: [_ | t], tokens: ["drop" | next]} = forth) do
+    eval_token(%{forth | stack: t, tokens: next})
   end
 
-  defp eval_token(%__MODULE__{stack: [b, a | _], tokens: ["swap" | rest]} = forth) do
+  defp eval_token(%__MODULE__{stack: [b, a | _], tokens: ["swap" | next]} = forth) do
     eval_token(%{
       forth
       | stack:
           List.replace_at(forth.stack, 0, a)
           |> List.replace_at(1, b),
-        tokens: rest
+        tokens: next
     })
   end
 
   defp eval_token(%__MODULE__{tokens: ["swap" | _]}),
     do: eval_token("Not enough elements in stack for operaiton: SWAP")
 
-  defp eval_token(%__MODULE__{stack: [_b, a | _], tokens: ["over" | rest]} = forth) do
-    eval_token(%{forth | stack: [a | forth.stack], tokens: rest})
+  defp eval_token(%__MODULE__{stack: [_b, a | _], tokens: ["over" | next]} = forth) do
+    eval_token(%{forth | stack: [a | forth.stack], tokens: next})
   end
 
   defp eval_token(%__MODULE__{tokens: ["over" | _]}),
     do: eval_token("Not enough elements in stack for operaiton: OVER")
 
-  defp eval_token(%__MODULE__{stack: stack, tokens: [token | rest]} = forth) do
+  defp eval_token(%__MODULE__{tokens: [":" | next]} = forth) do
+    # collecting tokens just before semicolon
+    custom_def_tokens =
+      Enum.reduce_while(next, [], fn next_token, custom_def ->
+        if next_token == ";" do
+          {:halt, custom_def}
+        else
+          {:cont, custom_def ++ [next_token]}
+        end
+      end)
+
+    cond do
+      custom_def_tokens == next ->
+        eval_token("Ending semicolon not found for custom word definition")
+
+      Enum.empty?(custom_def_tokens) ->
+        eval_token("Empty definition")
+
+      true ->
+        [custom_word_name | definitions] = custom_def_tokens
+
+        eval_token(%{
+          forth
+          | custom_words: add_custom_words(forth.custom_words, custom_word_name, definitions),
+            # taking the tokens after the `;`
+            tokens:
+              Enum.slice(
+                next,
+                # including the `;`
+                Enum.count(custom_def_tokens) + 1,
+                Enum.count(next) - Enum.count(custom_def_tokens) + 1
+              )
+        })
+    end
+  end
+
+  defp eval_token(%__MODULE__{stack: stack, tokens: [token | next]} = forth) do
     cond do
       number?(token) ->
-        eval_token(%{forth | stack: [String.to_integer(token) | stack], tokens: rest})
+        eval_token(%{forth | stack: [String.to_integer(token) | stack], tokens: next})
+
+      custom_word?(forth, token) ->
+        eval_token(%{forth | tokens: forth.custom_words[token] ++ next})
 
       true ->
         eval_token("No OP defined")
@@ -105,18 +144,11 @@ defmodule Forth do
     |> Enum.all?(fn digit -> digit >= 48 and digit <= 57 end)
   end
 
-  # TODO: Add the required stack size check
-
-  # defp eval_token(%__MODULE__{} = forth, ":") do
-  #   # start of word definition.
-  #   # we basically have to peek until the semicolon
-  # end
-
   @spec eval_arithmetic(String.t(), list(integer())) :: list(integer()) | String.t()
   defp eval_arithmetic(operation, stack) when length(stack) >= 2 do
     [b, a | _] = stack
 
-    # TODO: A chance to optimize below redundant code
+    # TODO: A chance to refactor below redundant code
     case operation do
       "+" ->
         List.delete_at(stack, 0)
@@ -142,4 +174,22 @@ defmodule Forth do
 
   defp eval_arithmetic(operation, _),
     do: "Not enough elements in stack for operation: #{operation}"
+
+  defp custom_word?(%__MODULE__{} = forth, token) do
+    Map.has_key?(forth.custom_words, token)
+  end
+
+  defp add_custom_words(custom_words, custom_word_name, definitions) do
+    defs =
+      case Enum.find_index(definitions, fn token -> token == custom_word_name end) do
+        nil ->
+          definitions
+
+        # inlining the redundant token to avoid infinite loop
+        index ->
+          List.replace_at(definitions, index, custom_words[custom_word_name]) |> List.flatten()
+      end
+
+    Map.put(custom_words, String.downcase(custom_word_name), defs)
+  end
 end
--- test/forth_test.exs
+++ test/forth_test.exs
@@ -142,4 +142,52 @@ defmodule ForthTest do
              Forth.new()
              |> Forth.eval("10 over")
   end
+
+  describe "user defined word definitions" do
+    @tag :custom
+    test "basic built-in def" do
+      assert {:ok, %{custom_words: %{"double-dup" => ["dup", "dup"]}}} =
+               Forth.new()
+               |> Forth.eval(": double-dup dup dup ;")
+    end
+
+    @tag :custom
+    test "no ending semicolon" do
+      assert {:error, "Ending semicolon not found for custom word definition"} =
+               Forth.new()
+               |> Forth.eval(": double-dup dup dup")
+    end
+
+    @tag :custom
+    test "empty definition error" do
+      assert {:error, "Empty definition"} =
+               Forth.new()
+               |> Forth.eval(": ;")
+    end
+
+    @tag :custom_inv
+    test "basic built-in def invocation" do
+      assert {:ok, %{custom_words: %{"double-dup" => ["dup", "dup"]}} = forth} =
+               Forth.new()
+               |> Forth.eval(": double-dup dup dup ;")
+
+      assert {:ok, %{stack: [1, 1, 1]}} = Forth.eval(forth, "1 double-dup")
+    end
+
+    @tag :custom_inv
+    test "basic built-in def invocation-2, add_num" do
+      assert {:ok, %{custom_words: %{"add_num" => ["1", "2", "3"]}} = forth} =
+               Forth.new()
+               |> Forth.eval(": add_num 1 2 3 ;")
+
+      assert {:ok, %{stack: [1, 1, 2, 3]}} = Forth.eval(forth, "1 add_num")
+    end
+  end
+
+  @tag :custom_inv
+  test "multiple definitions and invocations" do
+    assert {:ok, %{custom_words: %{"ten" => ["1"], "duplicate" => ["dup"]}, stack: [2]} = forth} =
+             Forth.new()
+             |> Forth.eval(": ten 1 ; : duplicate dup ; ten duplicate +")
+  end
 end
