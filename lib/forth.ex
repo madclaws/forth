856257cf90815e37a93c2773e7fc4f46cb876af6
madclaws <pavanan.anandu63@gmail.com>
2024-01-11 12:29:40 +0530

    feat: basic arithmetic ops done

--- lib/forth.ex
+++ lib/forth.ex
@@ -3,16 +3,96 @@ defmodule Forth do
   Documentation for `Forth`.
   """
 
-  @doc """
-  Hello world.
+  # TODO: typedoc
+  @type t :: %__MODULE__{
+          stack: list(integer())
+        }
 
-  ## Examples
+  defstruct stack: []
 
-      iex> Forth.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @spec new :: __MODULE__.t()
+  def new do
+    %__MODULE__{}
   end
+
+  @spec stack(__MODULE__.t()) :: list(integer())
+  def stack(%__MODULE__{stack: stack}), do: stack
+
+  @spec eval(__MODULE__.t(), String.t()) :: {:ok, __MODULE__.t()} | {:error, String.t()}
+  def eval(%__MODULE__{stack: stack}, input) do
+    String.split(input)
+    |> Enum.reduce_while(stack, fn data, stack ->
+      case eval_token(stack, data) do
+        res when is_binary(res) -> {:halt, {:error, res}}
+        res -> {:cont, res}
+      end
+    end)
+    |> case do
+      res when is_list(res) ->
+        {:ok, %__MODULE__{stack: Enum.reverse(res)}}
+
+      err ->
+        err
+    end
+  end
+
+  @spec eval_token(stack :: list(), token :: String.t()) :: list(integer()) | String.t()
+  defp eval_token(stack, token) do
+    cond do
+      number?(token) ->
+        [String.to_integer(token) | stack]
+
+      true ->
+        eval_word(stack, token)
+    end
+  end
+
+  # numbers should be noe or more ascii digits
+  @spec number?(String.t()) :: boolean()
+  defp number?(num) do
+    String.to_charlist(num)
+    |> Enum.all?(fn digit -> digit >= 48 and digit <= 57 end)
+  end
+
+  # TODO: Add the required stack size check
+  @spec eval_word(list(integer()), String.t()) :: stack :: list(integer()) | String.t()
+  defp eval_word(stack, token) do
+    IO.inspect(stack)
+
+    case token do
+      token when token in ["+", "-", "/", "*"] -> eval_arithmetic(token, stack)
+      "DUP" -> stack
+      "DROP" -> stack
+      "SWAP" -> stack
+      "OVER" -> stack
+    end
+  end
+
+  @spec eval_arithmetic(String.t(), list(integer())) :: list(integer()) | String.t()
+  defp eval_arithmetic(operation, stack) when length(stack) >= 2 do
+    [b, a | _] = stack
+
+    # TODO: A chance to optimize below redundant code
+    case operation do
+      "+" ->
+        List.delete_at(stack, 0)
+        |> List.replace_at(0, a + b)
+      "-" ->
+        List.delete_at(stack, 0)
+        |> List.replace_at(0, a - b)
+      "*" ->
+        List.delete_at(stack, 0)
+        |> List.replace_at(0, a * b)
+      "/" ->
+        if b == 0 do
+          "Division by zero is invalid"
+        else
+          List.delete_at(stack, 0)
+          |> List.replace_at(0, div(a, b))
+        end
+    end
+  end
+
+  defp eval_arithmetic(operation, _),
+    do: "Not enough elements in stack for operation: #{operation}"
 end
--- test/forth_test.exs
+++ test/forth_test.exs
@@ -2,7 +2,63 @@ defmodule ForthTest do
   use ExUnit.Case
   doctest Forth
 
-  test "greets the world" do
-    assert Forth.hello() == :world
+  # TODO: handling of extra spaces in eval?
+
+  test "empty stack" do
+    assert Forth.new() |> Forth.stack() == []
+  end
+
+  test "addition, success" do
+    assert {:ok, %{stack: [3]}} = Forth.new()
+    |> Forth.eval("1 2 +")
+  end
+
+  test "addition, failed" do
+    assert {:error, _} = Forth.new()
+    |> Forth.eval("1 +")
+  end
+
+  test "subtraction, success" do
+    assert {:ok, %{stack: [-8]}} = Forth.new()
+    |> Forth.eval("2 10 -")
+  end
+
+  test "subtraction, failed" do
+    assert {:error, _} = Forth.new()
+    |> Forth.eval("10 -")
+  end
+
+  test "multiplication, success" do
+    assert {:ok, %{stack: [40]}} = Forth.new()
+    |> Forth.eval("20 2 *")
+  end
+
+  test "multiplication, failed" do
+    assert {:error, _} = Forth.new()
+    |> Forth.eval("20 *")
+  end
+
+  test "division, success" do
+    assert {:ok, %{stack: [10]}} = Forth.new()
+    |> Forth.eval("20 2 /")
+  end
+
+  test "division, failed" do
+    assert {:error, "Division by zero is invalid"} = Forth.new()
+    |> Forth.eval("20 0 /")
+  end
+
+  test "multiple arithmetic operations" do
+    assert {:ok, %{stack: [-10]}} = Forth.new()
+    |> Forth.eval("10 20 + 40 -")
+
+    assert {:ok, %{stack: [2]}} = Forth.new()
+    |> Forth.eval("2 4 + 3 /")
+
+    assert {:ok, %{stack: [5]}} = Forth.new()
+    |> Forth.eval("10 20 * 40 /")
+
+    assert {:error, _} = Forth.new()
+    |> Forth.eval("10 20 * 0 /")
   end
 end
